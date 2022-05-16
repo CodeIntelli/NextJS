50fa935c5f44a4858770ba3759a0f5f2aae34a10
DUSHYANT KHODA <dushyant.k@upsquare.in>
2022-05-16 19:01:22 +0530

    link component incomplete

---
+++ 9-link-component/pages/index.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          background: "#000",
+          color: "#fff",
+        }}
+      >
+        Link Component In NEXT.JS
+      </h1>
+    </>
+  );
+}
